a38d19226b12206e26affb850adb48903bc7bc6b
Zadrakos <fromdoom@gmail.com>
2019-10-14 05:10:59 +0100

    Initial Commit.

--- src/main/java/com/zadrakos/strangerthings/StrangerThings.java
+++ src/main/java/com/zadrakos/strangerthings/StrangerThings.java
@@ -1,7 +1,11 @@
-package com.example.examplemod;
+package com.zadrakos.strangerthings;
 
+//import com.zadrakos.strangerthings.lists.ItemList;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,13 +23,14 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("strangerthings")
+public class StrangerThings
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    public static String modid = "strangerthings";
 
-    public ExampleMod() {
+    public StrangerThings() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -54,7 +59,7 @@ public class ExampleMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("strangerthings", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
@@ -79,6 +84,21 @@ public class ExampleMod
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
             // register a new block here
             LOGGER.info("HELLO from Register Block");
+        }
+
+        @SubscribeEvent
+        //public static void registerItems(final RegistryEvent.Register<Item> event)
+        //{
+        //    event.getRegistry().registerAll(
+        //            ItemList.saddle_body = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(location("saddle_body"))
+        //    );
+//
+        //    LOGGER.info("Items registered.");
+        //}
+
+        private static ResourceLocation location(String name)
+        {
+            return new ResourceLocation(modid, name);
         }
     }
 }
